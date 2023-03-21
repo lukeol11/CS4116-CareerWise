decf2baada58bc53cb6f2e9fc1f74c7ccc5fbfa9
example <example@example.ie>
2023-03-21 20:46:48 +0000

    style

--- navBar.php
+++ navBar.php
@@ -3,7 +3,7 @@
     <div class="navbar-inner">
         <a class="brand" href="#">careerWise</a>
         <ul class="nav">
-            <li ><a href="#">People</a></li>
+            <li><a href="#">People</a></li>
             <li><a href="#">Companies</a></li>
             <li><a href="#">Activity</a></li>
             <li><a href="#">Opportunities</a></li>
