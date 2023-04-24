75a375387c51a174962b7c58f9e1779be8321115
Luke O'Leary <luke.oleary1@gmail.com>
2023-04-24 22:28:41 +0100

    feat: added search functionality

--- people.php
+++ people.php
@@ -19,6 +19,16 @@
         </div>
 
         <div id="content">
+            <div id="search">
+                <form action="" method="get">
+                    <input type="text" name="query" placeholder="Search">
+                    <button type="submit">Go</button>
+                </form>
+            </div>
+
+        </div>
+
+        <div id="content">
             <div id="people">
                 <h2> People </h2>
                 <?php
@@ -28,7 +38,17 @@
                 $databaseName = "epiz_33784251_cs4116";
                 $connection = mysqli_connect($hostName, $userName, $password, $databaseName);
 
-                $query = "SELECT * FROM users";
+                // Check if a search query was submitted
+                if (isset($_GET["query"])) {
+                    $search_query = $_GET["query"];
+
+                    // request based on search query
+                    $query = "SELECT * FROM users WHERE FirstName LIKE '%$search_query%' OR LastName LIKE '%$search_query%' OR company LIKE '%$search_query%'";
+                } else {
+                    // else show all
+                    $query = "SELECT * FROM users";
+                }
+
                 $result = mysqli_query($connection, $query);
 
                 while ($row = mysqli_fetch_assoc($result)) {
@@ -43,9 +63,11 @@
                 ?>
 
 
+
             </div>
 
         </div>
+
     </div>
 </body>
 
