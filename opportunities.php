869d42261247ab59731a9b2643b71e0445714c2a
Cathalor-01 <20266235@studentmail.ul.ie>
2023-04-24 14:30:37 +0100

    changed company id to company name

--- opportunities.php
+++ opportunities.php
@@ -35,14 +35,14 @@
 
             if(isset($_POST['submit'])){
                 $search = $_POST['search'];
-                $query = "SELECT * FROM vacancies WHERE position LIKE '%$search%' OR salary_range LIKE '%$search%' OR company_id LIKE '%$search%' OR vacancy_id LIKE '%$search%'";
+                $query = "SELECT v.vacancy_id, b.company, v.position, v.salary_range FROM vacancies v JOIN business b ON v.company_id = b.company_id WHERE v.position LIKE '%$search%' OR v.salary_range LIKE '%$search%' OR b.company LIKE '%$search%' OR v.vacancy_id LIKE '%$search%'";
                 $result = mysqli_query($connection, $query);
                 $queryResult = mysqli_num_rows($result);
                 
                 if($queryResult > 0){
                     while ($row = mysqli_fetch_assoc($result)) {
                         echo "Vacancy: " . $row["vacancy_id"] . "<br>";
-                        echo "Company: " . $row["company_id"] . "<br>";
+                        echo "Company: " . $row["company"] . "<br>";
                         echo "Position: " . $row["position"] . "<br>";
                         echo "Salary Range: " . $row["salary_range"] . "<br>";
                         echo "<br><br>";
@@ -51,18 +51,18 @@
                     echo "There are no results matching your search!";
                 }
             } else {
-                $query = "SELECT * FROM vacancies";
+                $query = "SELECT v.vacancy_id, b.company, v.position, v.salary_range FROM vacancies v JOIN business b ON v.company_id = b.company_id";
                 $result = mysqli_query($connection, $query);
-
+            
                 while ($row = mysqli_fetch_assoc($result)) {
                     echo "Vacancy: " . $row["vacancy_id"] . "<br>";
-                    echo "Company: " . $row["company_id"] . "<br>";
+                    echo "Company: " . $row["company"] . "<br>";
                     echo "Position: " . $row["position"] . "<br>";
                     echo "Salary Range: " . $row["salary_range"] . "<br>";
                     echo "<br><br>";
                 }
             }
-
+            
             // Close the database connection
             mysqli_close($connection);
             ?>
