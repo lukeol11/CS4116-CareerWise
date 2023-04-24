68773a5a471d8de7780f02f59e9bed42939c06f5
Cathalor-01 <20266235@studentmail.ul.ie>
2023-04-24 02:21:53 +0100

    Added a basic search function to start

--- opportunities.php
+++ opportunities.php
@@ -8,6 +8,12 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=K2D:wght@100&display=swap" rel="stylesheet" />
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen" />
+    <style>
+        .search-form {
+            display: flex;
+            justify-content: center;
+        }
+    </style>
 </head>
 
 <body>
@@ -19,14 +25,32 @@
         </div>
 
         <div id="content">
-            <div id="people">
-                <?php
-                $hostName = "sql109.epizy.com";
-                $userName = "epiz_33784251";
-                $password = "XzX7r5XomWU";
-                $databaseName = "epiz_33784251_cs4116";
-                $connection = mysqli_connect($hostName, $userName, $password, $databaseName);
+        <div id="people">
+            <?php
+            $hostName = "sql109.epizy.com";
+            $userName = "epiz_33784251";
+            $password = "XzX7r5XomWU";
+            $databaseName = "epiz_33784251_cs4116";
+            $connection = mysqli_connect($hostName, $userName, $password, $databaseName);
 
+            if(isset($_POST['submit'])){
+                $search = $_POST['search'];
+                $query = "SELECT * FROM vacancies WHERE position LIKE '%$search%' OR salary_range LIKE '%$search%' OR company_id LIKE '%$search%' OR vacancy_id LIKE '%$search%'";
+                $result = mysqli_query($connection, $query);
+                $queryResult = mysqli_num_rows($result);
+                
+                if($queryResult > 0){
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "Vacancy: " . $row["vacancy_id"] . "<br>";
+                        echo "Company: " . $row["company_id"] . "<br>";
+                        echo "Position: " . $row["position"] . "<br>";
+                        echo "Salary Range: " . $row["salary_range"] . "<br>";
+                        echo "<br><br>";
+                    }
+                } else {
+                    echo "There are no results matching your search!";
+                }
+            } else {
                 $query = "SELECT * FROM vacancies";
                 $result = mysqli_query($connection, $query);
 
@@ -35,16 +59,22 @@
                     echo "Company: " . $row["company_id"] . "<br>";
                     echo "Position: " . $row["position"] . "<br>";
                     echo "Salary Range: " . $row["salary_range"] . "<br>";
-                    // echo "<a href='profilePage.php?userid=" . $row["user_id"] . "'>View Profile</a>";
                     echo "<br><br>";
                 }
+            }
 
-                // Close the database connection
-                mysqli_close($connection);
-                ?>
-            </div>
-
+            // Close the database connection
+            mysqli_close($connection);
+            ?>
         </div>
+        <form method="post" class="search-form">
+            <label>Search</label>
+            <input type="text" name="search">
+            <input type="submit" name="submit">
+        </form>
+    </div>
+</div>
+
     </div>
 </body>
 
