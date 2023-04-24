b0c0cf0b15f3508ffe917fba545d2ca06c170165
Luke O'Leary <luke.oleary1@gmail.com>
2023-04-24 22:44:31 +0100

    feat: added opportunities search and error checking

--- search.php
+++ search.php
@@ -44,17 +44,19 @@
                     // request based on search query
                     $query = "SELECT * FROM users WHERE FirstName LIKE '%$search_query%' OR LastName LIKE '%$search_query%' OR company LIKE '%$search_query%'";
                 } else {
-                    // else show all
-                    $query = "SELECT * FROM users";
+                    echo "Error";
                 }
 
                 $result = mysqli_query($connection, $query);
-
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "Name: " . $row["FirstName"] . " " . $row["LastName"] . "<br>";
-                    echo "Company: " . $row["company"] . "<br>";
-                    echo "<a href='profilePage.php?userid=" . $row["user_id"] . "'>View Profile</a>";
-                    echo "<br><br>";
+                if (mysqli_num_rows($result) == 0) {
+                    echo "There are no people matching your query :(";
+                } else {
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "Name: " . $row["FirstName"] . " " . $row["LastName"] . "<br>";
+                        echo "Company: " . $row["company"] . "<br>";
+                        echo "<a href='profilePage.php?userid=" . $row["user_id"] . "'>View Profile</a>";
+                        echo "<br><br>";
+                    }
                 }
 
                 // Close the database connection
@@ -79,26 +81,62 @@
 
                     // request based on search query
                     $query = "SELECT * FROM business WHERE company LIKE '%$search_query%'";
+                    $result = mysqli_query($connection, $query);
+                    if (mysqli_num_rows($result) == 0) {
+                        echo "There are no companies matching your query :(";
+                    } else {
+                        while ($row = mysqli_fetch_assoc($result)) {
+                            echo "Company: " . $row["company"] . "<br>";
+                            echo "<a href='companyPage.php?companyid=" . $row["company_id"] . "'>View Profile</a>";
+                            echo "<br><br>";
+                        }
+                    }
+
+    
+                    // Close the database connection
+                    mysqli_close($connection);
                 } else {
-                    // else show all businesses
-                    $query = "SELECT * FROM business";
+                    echo "Error";
                 }
 
-                $result = mysqli_query($connection, $query);
-
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "Company: " . $row["company"] . "<br>";
-                    echo "<a href='companyPage.php?companyid=" . $row["company_id"] . "'>View Profile</a>";
-                    echo "<br><br>";
-                }
-
-                // Close the database connection
-                mysqli_close($connection);
                 ?>
 
 
 
             </div>
+        </div>
+
+        <div id="content">
+        <div id="opportunities">
+            <h2> Opportunities </h2>
+            <?php
+            $hostName = "sql109.epizy.com";
+            $userName = "epiz_33784251";
+            $password = "XzX7r5XomWU";
+            $databaseName = "epiz_33784251_cs4116";
+            $connection = mysqli_connect($hostName, $userName, $password, $databaseName);
+
+            if (isset($_GET["query"])) {
+                $search = $_GET["query"];
+                $query = "SELECT b.company, v.position, v.salary_range FROM vacancies v JOIN business b ON v.company_id = b.company_id WHERE v.position LIKE '%$search%' OR v.salary_range LIKE '%$search%' OR b.company LIKE '%$search%'";
+                $result = mysqli_query($connection, $query);
+                $queryResult = mysqli_num_rows($result);
+                
+                if($queryResult > 0){
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "Company: " . $row["company"] . "<br>";
+                        echo "Position: " . $row["position"] . "<br>";
+                        echo "Salary Range: " . $row["salary_range"] . "<br>";
+                        echo "<br><br>";
+                    }
+                } else {
+                    echo "There are no opportunities matching your query :(";
+                }
+            }
+            
+            // Close the database connection
+            mysqli_close($connection);
+            ?>
         </div>
 
     </div>
