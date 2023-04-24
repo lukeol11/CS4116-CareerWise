c6a02f7608d1c635b5310b770fc7dcd979d0385c
Luke O'Leary <luke.oleary1@gmail.com>
2023-04-24 18:50:55 +0100

    style: changed where echo outputs

--- createVacancy.php
+++ createVacancy.php
@@ -50,54 +50,54 @@
           <button class="btn btn-primary" name="submit" type="submit">Submit</button>
         </form>
 
+        <?php
+        $dbhost = "sql109.epizy.com";
+        $dbuser = "epiz_33784251";
+        $dbpass = "XzX7r5XomWU";
+        $dbname = "epiz_33784251_cs4116";
+
+        $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+        if (mysqli_connect_errno()) {
+          die('Could not connect: ' . mysqli_connect_error());
+        }
+
+        if (isset($_POST['submit'])) {
+          $company = mysqli_real_escape_string($conn, $_POST['company']);
+          $position = mysqli_real_escape_string($conn, $_POST['position']);
+          $salary_range = mysqli_real_escape_string($conn, $_POST['salary_range']);
+
+          // Retrieve the company_id from the business table based on the provided company name
+          $sql = "SELECT company_id FROM business WHERE company = '$company'";
+          $result = mysqli_query($conn, $sql);
+
+          if ($result && mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            $company_id = $row['company_id'];
+
+            session_start();
+            $userId = $_SESSION['user_id'];
+
+            // Insert the new record into the vacancies table using the retrieved company_id
+            $sql = "INSERT INTO vacancies (company_id, position, salary_range) VALUES ('$company_id', '$position', '$salary_range')";
+            $result = mysqli_query($conn, $sql);
+
+            if ($result) {
+              echo "Post Made successfully!";
+              echo "Return to <a href='opportunities.php'>Opportunities</a>";
+            } else {
+              echo "Error: " . mysqli_error($conn) . " (" . mysqli_errno($conn) . ")";
+            }
+          } else {
+            echo "Error: Company not found";
+          }
+
+          mysqli_close($conn);
+        }
+        ?>
       </div>
     </div>
   </div>
-  <?php
-  $dbhost = "sql109.epizy.com";
-  $dbuser = "epiz_33784251";
-  $dbpass = "XzX7r5XomWU";
-  $dbname = "epiz_33784251_cs4116";
-
-  $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-  if (mysqli_connect_errno()) {
-    die('Could not connect: ' . mysqli_connect_error());
-  }
-
-  if (isset($_POST['submit'])) {
-    $company = mysqli_real_escape_string($conn, $_POST['company']);
-    $position = mysqli_real_escape_string($conn, $_POST['position']);
-    $salary_range = mysqli_real_escape_string($conn, $_POST['salary_range']);
-
-    // Retrieve the company_id from the business table based on the provided company name
-    $sql = "SELECT company_id FROM business WHERE company = '$company'";
-    $result = mysqli_query($conn, $sql);
-
-    if ($result && mysqli_num_rows($result) > 0) {
-      $row = mysqli_fetch_assoc($result);
-      $company_id = $row['company_id'];
-
-      session_start();
-      $userId = $_SESSION['user_id'];
-
-      // Insert the new record into the vacancies table using the retrieved company_id
-      $sql = "INSERT INTO vacancies (company_id, position, salary_range) VALUES ('$company_id', '$position', '$salary_range')";
-      $result = mysqli_query($conn, $sql);
-
-      if ($result) {
-        echo "Post Made successfully!";
-        echo "Return to <a href='opportunities.php'>Opportunities</a>";
-      } else {
-        echo "Error: " . mysqli_error($conn) . " (" . mysqli_errno($conn) . ")";
-      }
-    } else {
-      echo "Error: Company not found";
-    }
-
-    mysqli_close($conn);
-  }
-  ?>
 
 </body>
 
